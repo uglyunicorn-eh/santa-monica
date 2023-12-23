4a95be728cec8651e53b575224d62e3697960291
Pavel Reznikov <pashka.reznikov@gmail.com>
2023-12-23 12:21:12 -0800

    Remove ProfilingIntegration and update dependencies

--- src/lambda.ts
+++ src/lambda.ts
@@ -1,5 +1,5 @@
 import serverlessExpress from '@codegenie/serverless-express';
-import { ProfilingIntegration } from "@sentry/profiling-node";
+// import { ProfilingIntegration } from "@sentry/profiling-node";
 import Sentry from "@sentry/serverless";
 import 'source-map-support/register';
 import { app } from '.';
@@ -11,13 +11,13 @@ let lambdaHandler: any = serverlessExpress({ app });
 if (SENTRY_DSN) {
   Sentry.AWSLambda.init({
     dsn: SENTRY_DSN,
-    integrations: [
-      new ProfilingIntegration(),
-    ],
-    // Performance Monitoring
-    tracesSampleRate: 1.0,
-    // Set sampling rate for profiling - this is relative to tracesSampleRate
-    profilesSampleRate: 1.0,
+    // integrations: [
+    //   new ProfilingIntegration(),
+    // ],
+    // // Performance Monitoring
+    // tracesSampleRate: 1.0,
+    // // Set sampling rate for profiling - this is relative to tracesSampleRate
+    // profilesSampleRate: 1.0,
   });
   lambdaHandler = Sentry.AWSLambda.wrapHandler(lambdaHandler);
 }
