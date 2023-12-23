521285eace53fc60b5533ed84a48737ed47bd3a3
Pavel Reznikov <pashka.reznikov@gmail.com>
2023-12-23 12:56:20 -0800

    Fix Sentry import in lambda.ts

--- src/lambda.ts
+++ src/lambda.ts
@@ -1,6 +1,6 @@
 import serverlessExpress from '@codegenie/serverless-express';
 // import { ProfilingIntegration } from "@sentry/profiling-node";
-// import Sentry from "@sentry/serverless";
+import Sentry from "@sentry/serverless";
 import 'source-map-support/register';
 import { app } from '.';
 
@@ -9,17 +9,17 @@ const SENTRY_DSN = process.env.SENTRY_DSN;
 let lambdaHandler: any = serverlessExpress({ app });
 
 if (SENTRY_DSN) {
-  // Sentry.AWSLambda.init({
-  //   dsn: SENTRY_DSN,
-  //   // integrations: [
-  //   //   new ProfilingIntegration(),
-  //   // ],
-  //   // // Performance Monitoring
-  //   // tracesSampleRate: 1.0,
-  //   // // Set sampling rate for profiling - this is relative to tracesSampleRate
-  //   // profilesSampleRate: 1.0,
-  // });
-  // lambdaHandler = Sentry.AWSLambda.wrapHandler(lambdaHandler);
+  Sentry.AWSLambda.init({
+    dsn: SENTRY_DSN,
+    //   // integrations: [
+    //   //   new ProfilingIntegration(),
+    //   // ],
+    //   // // Performance Monitoring
+    //   // tracesSampleRate: 1.0,
+    //   // // Set sampling rate for profiling - this is relative to tracesSampleRate
+    //   // profilesSampleRate: 1.0,
+  });
+  lambdaHandler = Sentry.AWSLambda.wrapHandler(lambdaHandler);
 }
 
 export const handler = lambdaHandler;
