04ce5a978e1ce4c6ab4a1c44d62f2699d1448ac4
Pavel Reznikov <pashka.reznikov@gmail.com>
2023-12-23 15:53:11 -0800

    Update Sentry integrations and error handling

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-import { RewriteFrames } from '@sentry/integrations';
 import * as Sentry from '@sentry/node';
 import cors from "cors";
 import dotenv from "dotenv";
@@ -23,11 +22,20 @@ if (SENTRY_DSN) {
     dsn: SENTRY_DSN,
     release: `${PACKAGE.name}@${PACKAGE.version}`,
     environment: process.env.NODE_ENV,
-    integrations: [new RewriteFrames()],
+    integrations: [
+      // enable HTTP calls tracing
+      new Sentry.Integrations.Http({ tracing: true }),
+      // enable Express.js middleware tracing
+      new Sentry.Integrations.Express({ app }),
+    ],
     tracesSampleRate: 1.0,
-    profilesSampleRate: 1.0,
   });
+
+  // The request handler must be the first middleware on the app
   app.use(Sentry.Handlers.requestHandler());
+
+  // TracingHandler creates a trace for every incoming request
+  app.use(Sentry.Handlers.tracingHandler());
 }
 
 app.use(morgan("dev"));
@@ -50,7 +58,16 @@ app.get("/error", (req: Request, res: Response) => {
 
 
 if (SENTRY_DSN) {
+  // The error handler must be registered before any other error middleware and after all controllers
   app.use(Sentry.Handlers.errorHandler());
+
+  // Optional fallthrough error handler
+  // app.use(function onError(err, req, res, next) {
+  //   // The error id is attached to `res.sentry` to be returned
+  //   // and optionally displayed to the user for support.
+  //   res.statusCode = 500;
+  //   res.end(res.sentry + "\n");
+  // });
 }
 
 if (port) {
