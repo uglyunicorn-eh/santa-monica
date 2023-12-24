ba544e22730d759b0b1cd08c423a36d5214e6c96
Pavel Reznikov <pashka.reznikov@gmail.com>
2023-12-23 22:34:20 -0800

    Refactor error handling and remove unnecessary code

--- src/index.ts
+++ src/index.ts
@@ -9,10 +9,9 @@ import { commonHeaders } from "src/utils/express/commonHeaders";
 
 import PACKAGE from 'package.json';
 
-const SENTRY_DSN = process.env.SENTRY_DSN;
-
 dotenv.config();
 
+const SENTRY_DSN = process.env.SENTRY_DSN;
 const port = process.env.PORT;
 
 export const app: Express = express();
@@ -23,18 +22,12 @@ if (SENTRY_DSN) {
     release: `${PACKAGE.name}@${PACKAGE.version}`,
     environment: process.env.NODE_ENV,
     integrations: [
-      // enable HTTP calls tracing
       new Sentry.Integrations.Http({ tracing: true }),
-      // enable Express.js middleware tracing
       new Sentry.Integrations.Express({ app }),
     ],
     tracesSampleRate: 1.0,
   });
-
-  // The request handler must be the first middleware on the app
   app.use(Sentry.Handlers.requestHandler());
-
-  // TracingHandler creates a trace for every incoming request
   app.use(Sentry.Handlers.tracingHandler());
 }
 
@@ -52,16 +45,7 @@ app.get("/", (req: Request, res: Response) => {
 
 
 if (SENTRY_DSN) {
-  // The error handler must be registered before any other error middleware and after all controllers
   app.use(Sentry.Handlers.errorHandler());
-
-  // Optional fallthrough error handler
-  // app.use(function onError(err, req, res, next) {
-  //   // The error id is attached to `res.sentry` to be returned
-  //   // and optionally displayed to the user for support.
-  //   res.statusCode = 500;
-  //   res.end(res.sentry + "\n");
-  // });
 }
 
 if (port) {
