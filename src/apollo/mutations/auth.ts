6780e4fe2f89fc1d965d7631ca663c6cf8bfc36a
Pavel Reznikov <pashka.reznikov@gmail.com>
2023-12-25 20:57:00 -0800

    Add RSA private key parameter to deploy script

--- src/apollo/mutations/auth.ts
+++ src/apollo/mutations/auth.ts
@@ -4,19 +4,21 @@ import { ApolloContext } from 'src/apollo/context';
 import { enterInputSchema } from './validation';
 import { EnterInput } from './types';
 
-import { baseUrl } from 'src/config.json';
+import { baseUrl, sendgridTemplates } from 'src/config.json';
 
 export default {
   auth: () => ({
 
     enter: _(enterInputSchema)(async (input: EnterInput, { db, user, sendMail }: ApolloContext) => {
       if (!user) {
+        const token = '';
+
         await sendMail(
-          'd-2b7845c2dfec4068aa2d25496516262f',
+          sendgridTemplates.signIn,
           {
             to: input.email,
             dynamicTemplateData: {
-              magicLink: `${baseUrl}/link/NBGYo78puio.HGF&Ttghhg5678il.sfasdh`,
+              magicLink: `${baseUrl}/enter/${token}`,
             },
           },
         );
